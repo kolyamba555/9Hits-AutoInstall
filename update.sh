1bc851de95c92d262ae1362707962b94277ac4ae
kolyamba555 <122336884+kolyamba555@users.noreply.github.com>
2023-10-28 13:09:44 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/9Hits/
 /root/kill.sh
-wget http://dl.9hits.com/patch-v3-linux64.tar.bz2
+wget http://mirror-dl.9hits.com/patch-v3-linux64.tar.bz2
 cd /root/9Hits/9HitsViewer_x64/
 unzip 9h-patch-linux-x64.zip
 cd /root/9Hits/
